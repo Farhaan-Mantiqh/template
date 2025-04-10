865b7d80e3bd7d1b14156811076abd1276caa8cc
Farhaan.mantiqh <farhaan.mantiqh@gmail.com>
2025-04-10 11:30:17 +0530

    fix: correct typo in README

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,20 @@
-// commitlint.config.js
 module.exports = {
   rules: {
     'type-enum': [
       2,
       'always',
       [
-        'feature', // new feature
-        'fix', // bug fix
-        'chore', // tooling, maintenance
-        'docs', // documentation
-        'refactor', // code restructuring
-        'test', // test-related
-        'ci', // CI/CD pipeline
-        'api', // API changes
-        'ui', // frontend/UI updates
-        'style', // UI Update
-        'initial', // Initial Commit
+        'feature',
+        'fix',
+        'chore',
+        'docs',
+        'refactor',
+        'test',
+        'ci',
+        'api',
+        'ui',
+        'style',
+        'initial',
       ],
     ],
   },
